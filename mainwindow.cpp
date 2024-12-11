7d71c0e196658ac3614f2ece79bbeece508f7ce5
d1mas334 <drondmitrser334@gmail.com>
2024-12-11 16:48:36 +0300

    Вывод таблицы
    Сформирован вывод данных в таблицу.
    Релизована функция вывода на таблицу

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,77 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    db.setDatabaseName("DRIVER={ODBC Driver 17 for SQL Server};SERVER=LAPTOP-82D8N0HS\\SQLEXPRESS;"
+                       "DATABASE=Aircrafts;Trusted_connection=yes");
+    if(!db.open()){
+        qDebug() << "Код ошибки: " << db.lastError().text();
+        this->close();
+    }
+
+    QSqlQuery *q = new QSqlQuery;
+    q->exec(QString("SELECT [id], [Название], [Тип ВС], [Страна], [Дата начала эксплуатации],"
+                            "[Дата окончания эксплуатации] FROM mt1;"));
+    fillingTable(q);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::fillingTable(QSqlQuery *q){
+    ui->tableWidget->setColumnCount(6); // Указываем число колонок
+    ui->tableWidget->setShowGrid(true); // Включаем сетку
+    // Разрешаем выделение только одного элемента
+    ui->tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+    // Разрешаем выделение построчно
+    ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+    // Устанавливаем заголовки колонок
+    //QStringList headers;
+
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList()<<QString::fromStdString("id")
+                                                             <<QString::fromStdString("Название")
+                                                             <<QString::fromStdString("Тип ВС")
+                                                             <<QString::fromStdString("Страна")
+                                                             <<QString::fromStdString("Дата начала эксплуатации")
+                                                             <<QString::fromStdString("Дата окончания эксплуатации"));
+    // Растягиваем последнюю колонку на всё доступное пространство
+    //ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+    // Скрываем колонку под номером 0
+    ui->tableWidget->hideColumn(0);
+    int i = 0;
+    for(/*int*/ i = 0; q->next(); i++){
+        // Вставляем строку
+        ui->tableWidget->insertRow(i);
+        /* Устанавливаем в первую колонку id забирая его из результата SQL-запроса
+         * Эта колонка будет скрыта
+         * */
+        ui->tableWidget->setItem(i,0, new QTableWidgetItem(q->value(0).toString()));
+
+        // Создаём элемент, который будет выполнять роль чекбокса
+//        QTableWidgetItem *item = new QTableWidgetItem();
+//        item->data(Qt::CheckStateRole);
+        /* Проверяем, на статус нечетности, если нечетное устройство, то
+         * выставляем состояние чекбокса в Checked, иначе в Unchecked
+         * */
+//        if(q->value(1).toInt() == 1){
+//            item->setCheckState(Qt::Checked);
+//        } else {
+//            item->setCheckState(Qt::Unchecked);
+//        }
+        // Устанавливаем чекбокс во вторую колонку
+//        ui->tableWidget->setItem(i,1, item);
+        // Далее забираем все данные из результата запроса и устанавливаем в остальные поля
+        ui->tableWidget->setItem(i,1, new QTableWidgetItem(q->value(1).toString()));
+        ui->tableWidget->setItem(i,2, new QTableWidgetItem(q->value(2).toString()));
+        ui->tableWidget->setItem(i,3, new QTableWidgetItem(q->value(3).toString()));
+        ui->tableWidget->setItem(i,4, new QTableWidgetItem(q->value(4).toString()));
+        ui->tableWidget->setItem(i,5, new QTableWidgetItem(q->value(5).toString()));
+    }
+    if (i == 0){
+        qDebug()<<"Mistake";
+    }
+}
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "filter.h"
+#include "QSqlDatabase"
+// #include <QSqlRelationalTableModel>
+#include "QSqlError"
+#include "QSqlQuery"
+// #include <QSqlQueryModel>
+// #include "QSqlTableModel"
+#include "information.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +23,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+        void fillingTable(QSqlQuery *q);
+
 private:
     Ui::MainWindow *ui;
+    Filter f;
+    Information infWindow;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    bool admin = true;
+    std::string title = "", atype = "", yearst = "1900", yearend = "2024", country = "";
+    int vppmin = 0, vppmax = 9999999, pasmin = 0, pasmax = 9999;
 };
 #endif // MAINWINDOW_H
